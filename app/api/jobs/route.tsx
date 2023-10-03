081ba38680a687afdd555da12f46689c10d3cea3
al <eng.alkhateebali@gmail.com>
2023-10-03 23:23:35 +0000

    Adding Icons

--- app/api/jobs/route.tsx
+++ app/api/jobs/route.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-import { Search } from "@/app/types/jobs/Job";
 
 interface Props {
   searchParams:  {
--- app/components/searchBox/SearchBox.tsx
+++ app/components/searchBox/SearchBox.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Briefcase from "./icons/Briefcase";
+import Location from "./icons/Location";
 import useSearch from "./useSearch";
 
 const SearchBox = () => {
@@ -7,9 +9,13 @@ const SearchBox = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-wrap  p-5">
         <div className="join join-vertical lg:w-2/6 lg:mr-2 md:mt-4">
-          <label htmlFor="what" className="text-lg pb-2">
-            What
-          </label>
+          <div className="join join-horizontal">
+            <label htmlFor="what" className="text-lg pb-2 mr-2">
+              What
+            </label>
+             <Briefcase/>
+          </div>
+
           <input
             type="text"
             placeholder="Job title, keywords or company"
@@ -18,9 +24,13 @@ const SearchBox = () => {
           />
         </div>
         <div className="join join-vertical lg:w-2/6 lg:ml-2 sm:mt-4 ">
-          <label htmlFor="where" className="text-lg pb-2">
+          <div  className="join join-horizontal" >
+          <label htmlFor="where" className="text-lg pb-2 pr-2">
             Where
           </label>
+          <Location />
+          </div>
+         
           <input
             type="text"
             placeholder="Town or region"
@@ -29,8 +39,10 @@ const SearchBox = () => {
           />
           <input hidden value={1} {...register("page")} />
         </div>
-        <div className="pt-4 lg:ml-4  sm:mt-4 w-2/9">
-          <button  type="submit" className="btn sm:w-full">Search</button>
+        <div className="pt-4 lg:ml-4  sm:mt-4 w-2/9 ">
+          <button type="submit" className="btn  sm:w-full  ">
+            Search
+          </button>
         </div>
       </div>
     </form>
