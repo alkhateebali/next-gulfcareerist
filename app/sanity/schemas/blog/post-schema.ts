c7ce9e9751a76ff6c7c4a43f9438c5a0e7540483
al <eng.alkhateebali@gmail.com>
2023-10-06 12:46:36 +0000

    Adding post schema

---
+++ app/sanity/schemas/blog/post-schema.ts
@@ -0,0 +1,45 @@
+
+const post = {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+    },
+    {
+      name: "featuredImage",
+      title: "Featured Image",
+      type: "image",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "url",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
+
+export default post;
