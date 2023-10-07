13e14eaaacfd60df4ce74d5339aa1260559dde3b
al <eng.alkhateebali@gmail.com>
2023-10-07 23:45:18 +0000

    edit some css

--- app/(site)/blog/PostCard.tsx
+++ app/(site)/blog/PostCard.tsx
@@ -13,11 +13,17 @@ const PostCard = ({ post }: Props) => {
   // console.log(post);
   return (
     <Link href={`blog/article/${post.slug}`}>
-      <div className="card w-dull bg-base-100 border-b border-gray-200  py-2   hover:bg-slate-50  transition">
+      <div className="card w-dull bg-base-100 border-b border-gray-200  my-2  hover:bg-slate-50  transition">
         <div className="card-body">
           <div className="grid grid-cols-4 gap-4">
             <div className="col-span-3">
-              <div className="join join-horizontal ">
+         
+              <h2 className="card-title ">{post.title}</h2>
+              <p className="hidden md:block text-gray-600  text-sm  pt-1 pb-6">
+                {post.introduction}
+              </p>
+                <div className="join join-horizontal ">
+            
                 <Image
                   src={post.authorImage}
                   className="rounded-full h-5 w-5  mr-2"
@@ -27,10 +33,6 @@ const PostCard = ({ post }: Props) => {
                 />
                 <span className="text-sm">{post.authorname}</span>
               </div>
-              <h2 className="card-title ">{post.title}</h2>
-              <p className="hidden md:block text-gray-600  text-sm  pt-1 pb-6">
-                {post.introduction}
-              </p>
             </div>
             <div className="col-span-1">
               <Image
@@ -43,17 +45,19 @@ const PostCard = ({ post }: Props) => {
             </div>
           </div>
 
-          <ul>
-            <span className="mr-8">
-              <FormatedDate date={post._createdAt.toString()} />
-            </span>
-            <span className="mr-2">Posted in</span>
-            {post.categories.map((category) => (
-              <li className="badge badge-ghost  gap-2 mr-2" key={category}>
-                {category}
-              </li>
-            ))}
-          </ul>
+          <div className="text-sm">
+            <ul>
+              <span className="mr-8">
+                <FormatedDate date={post._createdAt.toString()} />
+              </span>
+              <span className="mr-2">Posted in</span>
+              {post.categories.map((category) => (
+                <li className="badge badge-ghost  gap-2 mr-2" key={category}>
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </Link>
