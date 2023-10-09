5311c8daf79b5d42c56a831e416fc9047911bb25
al <eng.alkhateebali@gmail.com>
2023-10-09 18:02:22 +0000

    Designing Theme Switcher

--- app/(site)/components/ThemeSwitcher.tsx
+++ app/(site)/components/ThemeSwitcher.tsx
@@ -1,28 +1,42 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
-
-import { Button } from '@nextui-org/button'
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@nextui-org/button";
+import { Switch } from "@nextui-org/react";
+import { MoonIcon, SunIcon } from "./icons/AppIcons";
 
 export default function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
-
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+ 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if (!mounted) return null
-
+  if (!mounted) return null;
+  const selected = theme == "light" ? false : true;
+  const selectedHandler=()=>{
+    theme == "dark" ? setTheme('light') : setTheme('dark')
+  }
   return (
-    <div className='flex gap-4'>
-      <Button size='sm' variant='flat' onClick={() => setTheme('light')}>
-        Light
-      </Button>
-      <Button size='sm' variant='flat' onClick={() => setTheme('dark')}>
-        Dark
-      </Button>
-    </div>
-  )
+
+    <Switch
+    onChange={selectedHandler}
+      defaultSelected={selected}
+      size="sm"
+      color="warning"
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <SunIcon className={className} />
+        ) : (
+          <MoonIcon className={className} />
+        )
+      }
+      // startContent={<SunIcon />}
+      // endContent={<MoonIcon />}
+    >
+      Dark mode
+    </Switch>
+  );
 }
