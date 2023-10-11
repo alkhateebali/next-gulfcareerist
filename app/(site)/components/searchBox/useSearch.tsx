24873ef7d60514432bf3426efb1953c29edb51e6
al <eng.alkhateebali@gmail.com>
2023-10-11 21:51:18 +0000

    Fixsearch url bug

--- app/(site)/components/searchBox/useSearch.tsx
+++ app/(site)/components/searchBox/useSearch.tsx
@@ -5,9 +5,13 @@ import { useRouter } from "next/navigation";
 const useSearch = () => {
   const { register, handleSubmit } = useForm<Search>();
   const router = useRouter();
-  const onSubmit = (data: Search) => {
-    const queryString = `location=${data.location}&keywords=${data.keywords}&page=${data.page}`;
-    const url = `http://localhost:3000/search?${queryString}`;
+  const onSubmit = (searchParams: Search) => {
+    const SEARCH_URL =process.env.SEARCH_URL ?  process.env.SEARCH_URL : ""
+    const search= new URLSearchParams;
+    search.append('location',searchParams.location)
+    search.append('keywords',searchParams.keywords)
+    search.append('page',searchParams.page.toString())
+    const url = SEARCH_URL+ `/search?${search}`;
     router.push(url);
   };
   return { handleSubmit, onSubmit, register };
