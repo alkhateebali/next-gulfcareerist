c934c065b1eb05a8bfcdb223f8ca4ec0597e230b
al <eng.alkhateebali@gmail.com>
2024-06-26 16:28:59 +0000

    Modify social media links

--- app/(site)/layout/Footer.tsx
+++ app/(site)/layout/Footer.tsx
@@ -5,17 +5,25 @@ export const Footer = () => {
   return (
     <footer className="flex  flex-col  flex-wrap  items-center justify-center text-center p-10 bg-default-50  gab-4">
       <nav className="flex flex-nowrap  gap-4 py-1">
-        <Link className="link " href={'/'}>Search Jobs</Link>
-        <Link   className="link " href={'/blog'}>Blog</Link>
-        <Link   className="link " href={"/contact-us"}>Contact us</Link>
-        <Link   className="link " href={'/privacy-policy'}>Privacy Policy</Link>
+        <Link className="link " href={"/"}>
+          Search Jobs
+        </Link>
+        <Link className="link " href={"/blog"}>
+          Blog
+        </Link>
+        <Link className="link " href={"/contact-us"}>
+          Contact us
+        </Link>
+        <Link className="link " href={"/privacy-policy"}>
+          Privacy Policy
+        </Link>
       </nav>
       <nav>
         <div className="flex gap-4 py-4">
-          <Facebook link="https://www.facebook.com/gulfcareerist"/>
-          <LinkedIn link="https://www.linkedin.com/company/gulf-careerist/"/>
-          <Instagram link="https://www.instagram.com/gulfcareerist/"/>
-          <Telegram link="https://t.me/gulfcareerist_bot"/>
+          <Facebook link="https://www.facebook.com/gulfcareerist" />
+          <LinkedIn link="https://www.linkedin.com/company/" />
+          <Instagram link="https://www.instagram.com/" />
+          <Telegram link="https://t.me/" />
         </div>
       </nav>
       <aside>
